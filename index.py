8f79e0933e5bf4cf8f2656cdce683a0449b226d7
Asif <1421015@iub.edu.bd>
2022-03-14 16:46:17 +0600

    [done] model that can predict music genre

--- index.py
+++ index.py
@@ -1,24 +1,54 @@
-from typing import Optional
-
+import uvicorn
+import pickle
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
+class Music(BaseModel):
+    acousticness: float
+    danceability: float
+    energy: float
+    instrumentalness: float
+    liveness: float
+    speechiness: float
+    tempo: float
+    valence: float
+
+
 app = FastAPI()
 
-class Item (BaseModel):
-    name: str
-    price: str
-    is_offer: Optional [bool]=None 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+with open("./ml-model/music-genre.pkl", "rb") as f:
+    model = pickle.load(f)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/')
+def index():
+    return {'message': 'This is the homepage of the API '}
 
-@app.put("/items/{item_id}")
-def update_item(item_id:int, item:Item):
-    return {"item-nmae": item.name, "item_id":item_id }
+
+@app.post('/prediction')
+def get_music_category(data: Music):
+    received = data.dict()
+    acousticness = received['acousticness']
+    danceability = received['danceability']
+    energy = received['energy']
+    instrumentalness = received['instrumentalness']
+    liveness = received['liveness']
+    speechiness = received['speechiness']
+    tempo = received['tempo']
+    valence = received['valence']
+    pred_name = model.predict([[acousticness, danceability, energy,
+                                instrumentalness, liveness, speechiness, tempo, valence]]).tolist()[0]
+    return {'prediction': pred_name}
+
+
+@app.get('/predict')
+def get_cat(acousticness: float, danceability: float, energy: float, instrumentalness: float, liveness: float, speechiness: float, tempo: float, valence: float):
+    pred_name = model.predict([[acousticness, danceability, energy,
+            instrumentalness, liveness, speechiness, tempo, valence]]).tolist()[0]
+    return {'prediction': pred_name}
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)
