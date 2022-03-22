6dd2022a4b5a5ec12b8d2efd4493f9cb314031b6
Asif <1421015@iub.edu.bd>
2022-03-22 19:16:39 +0600

    [add] skeleton Home and Favourite

---
+++ client/src/container/Favourite.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import {Text} from 'react-native'
+
+const Favourite=()=>{
+    return <Text>Favourite</Text>
+}
+
+export default Favourite
--- client/src/navigation/Nav.js
+++ client/src/navigation/Nav.js
@@ -3,22 +3,15 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {Text} from 'react-native'
-//import Home from './src/container/Home';
-//import NewExpense from './src/component/home/NewExpense';
-//import Category from './src/container/Category';
+import Home from '../container/Home';
+import Favourite from '../container/Favourite';
 
-////////////////////////////////////////////////////////////////////////////////////
-const Home=()=>{
-    return <Text>Home</Text>
-}
 
 const NewExpense=()=>{
     return <Text>NewExpense</Text>
 }
 
-const Category=()=>{
-    return <Text>Category</Text>
-}
+////////////////////////////////////////////////////////////////////////////////////
 
 const Stack = createNativeStackNavigator();
 
@@ -39,7 +32,7 @@ export default function App() {
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="HomeStack">
         <Drawer.Screen name="HomeStack" component={HomeStack} />
-        <Drawer.Screen name="Category" component={Category} />
+        <Drawer.Screen name="Favourite" component={Favourite} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
