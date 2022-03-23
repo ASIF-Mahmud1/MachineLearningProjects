1f6d6e7b6a51c924ad2e6169d05cfe666da65c0f
Asif <1421015@iub.edu.bd>
2022-03-23 17:42:59 +0600

    [done] favourite added to AsyncStorage

--- client/src/container/Favourite.js
+++ client/src/container/Favourite.js
@@ -1,7 +1,23 @@
-import * as React from 'react';
-import {Text} from 'react-native'
+import React, { useEffect } from 'react';
+import { Text } from 'react-native'
+import { getData } from '../helper/helper';
+import { useIsFocused } from '@react-navigation/native';
 
-const Favourite=()=>{
+const Favourite = () => {
+    const isFoucsed = useIsFocused()
+    useEffect(() => {
+        if (isFoucsed) {
+            getData("favourite", (data) => {
+                if (data) 
+                {
+                    console.log(JSON.stringify(data));
+                  //  alert(data.length)
+                }
+            })
+        }
+
+    }, [isFoucsed])
+
     return <Text>Favourite</Text>
 }
 
