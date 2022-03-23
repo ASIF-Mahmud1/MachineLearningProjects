a3b5b97bb890543d05582e8ba8f9ea5603d4c302
Asif <1421015@iub.edu.bd>
2022-03-23 16:51:34 +0600

    [add] Indicator integrated

--- client/src/container/Home.js
+++ client/src/container/Home.js
@@ -5,6 +5,7 @@ import ConverterCell from '../component/home/ConverterCell';
 import Sentence from '../component/home/Sentence';
 import Translation from '../component/home/Translation';
 import { translate } from '../api/translate-api';
+import { Loader,showToast } from '../helper/component/Indicator';
 
 const Home=()=>{
     const [language, setLanguage]= useState({
@@ -17,18 +18,27 @@ const Home=()=>{
         translate:''
     })
 
+    const[loading,setLoading]=useState(false)
+
     const handleTranslate= async()=>{
+        setLoading(true)
         const result= await translate(sentence.orginal)
         if(result?.prediction)
         {
             console.log(sentence.orginal, result);
             setSentence({...sentence, translate: result.prediction})
         }
+        else 
+        {
+           showToast("Something went wrong")
+        }
+        setLoading(false)
      
     }
 
     return (
         <View style={styles.container}>
+              <Loader loading={loading}/>
               <ConverterCell />     
               <Sentence  sentence={sentence.orginal}  handleParentState= {(feedBack)=> setSentence({...sentence, orginal:feedBack}) } handleTranslate={handleTranslate} />
               <Translation  translate= {sentence.translate} />
